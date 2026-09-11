23737a63481ffa0cd5ebb6a475f787ca89bee1d5
Foul <foulweb@gmail.com>
2026-09-11 18:27:49 +0200

    WOPR 2.3.263 - Uniformiser les couleurs des actions quel que soit le thème

--- public/app.py
+++ public/app.py
@@ -99,7 +99,7 @@ GOOGLE_TOKEN = PRIVATE_ROOT / "data" / "google_token.json"
 SMTP_SETTINGS_FILE = PRIVATE_ROOT / "data" / "smtp_settings.json"
 ABBY_SETTINGS_FILE = PRIVATE_ROOT / "data" / "abby_settings.json"
 ABBY_API_BASE = "https://api.app-abby.com"
-APP_VERSION = "2.3.261"
+APP_VERSION = "2.3.263"
 GOOGLE_SCOPE = ["https://www.googleapis.com/auth/contacts"]
 
 # Sécurité locale WOPR
@@ -958,11 +958,14 @@ def force_utf8_html(response):
             google_sync_js_tag = f'<script src="/static/wopr-google-sync.js?v={APP_VERSION}" defer></script>'
             folders_v248_js_tag = f'<script src="/static/wopr-folders-v248.js?v={APP_VERSION}" defer></script>'
             uniform_css_tag = f'<link rel="stylesheet" href="/static/wopr-uniform-lists.css?v={APP_VERSION}">'
+            action_colors_css_tag = f'<link rel="stylesheet" href="/static/wopr-action-colors.css?v={APP_VERSION}">'
 
             if "wopr-responsive.css" not in html and "</head>" in html:
                 html = html.replace("</head>", css_tag + "\n</head>", 1)
             if "wopr-uniform-lists.css" not in html and "</head>" in html:
                 html = html.replace("</head>", uniform_css_tag + "\n</head>", 1)
+            if "wopr-action-colors.css" not in html and "</head>" in html:
+                html = html.replace("</head>", action_colors_css_tag + "\n</head>", 1)
 
 
             if "wopr-responsive.js" not in html and "</body>" in html:
